dd1645f94e4cde45b99d08802fcc036a2f5cf0f4
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-09-11 12:34:00 +0300

    dragging with mouse wheel, rotating with shift+arrow

--- lapa1/main.cpp
+++ lapa1/main.cpp
@@ -3,12 +3,23 @@
 #include "atlwin.h"
 #include "windows.h"
 #include "gdiplus.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 using namespace Gdiplus;
+
+#define ID_ACCEL_LEFT  5001
+#define ID_ACCEL_RIGHT 5002
+
+ACCEL accelTable[] = {
+        { FSHIFT | FVIRTKEY, VK_LEFT,  ID_ACCEL_LEFT},
+        { FSHIFT | FVIRTKEY, VK_RIGHT, ID_ACCEL_RIGHT}
+};
 
 const int IMAGE_WIDTH = 200;
 const int IMAGE_HEIGHT = 200;
 const int DRAGGING_OFFSET = 50;
+const int MOVE_SPEED = 10;
 const std::wstring PATH_TO_OPEN_MOUTH_CAT_IMAGE = L"C:\\Users\\madam\\CLionProjects\\sys-prog\\lapa1\\resources\\images\\catWithOpenMouse.jpg";
 const std::wstring PATH_TO_CLOSE_MOUTH_CAT_IMAGE = L"C:\\Users\\madam\\CLionProjects\\sys-prog\\lapa1\\resources\\images\\catWithCLoseMouse.jpg";
 
@@ -19,6 +30,7 @@ bool isImageChanged = false;
 
 int imageX = 50;
 int imageY = 50;
+int angle = 0;
 
 Image *LoadImageFromFile(const std::wstring &filePath) {
     return new Image(filePath.c_str());
@@ -27,12 +39,35 @@ Image *LoadImageFromFile(const std::wstring &filePath) {
 // Инициализация GDI+
 void InitGDIPlus(ULONG_PTR *gdiplusToken) {
     GdiplusStartupInput gdiplusStartupInput;
-    GdiplusStartup(gdiplusToken, &gdiplusStartupInput, NULL);
+    GdiplusStartup(gdiplusToken, &gdiplusStartupInput, nullptr);
 }
 
 // Завершение работы с GDI+
 void ShutdownGDIPlus(ULONG_PTR *gdiplusToken) {
     GdiplusShutdown(*gdiplusToken);
+}
+
+void UpdateSpritePosition(HWND hWnd) {
+    InvalidateRect(hWnd, nullptr, TRUE);
+}
+
+bool IsEnglishKeyboardLayout() {
+    HKL layout = GetKeyboardLayout(0);
+    return LOWORD(layout) == 0x409;  // 0x409 — код английского (США) языка
+}
+
+void CheckWindowBounds(HWND hWnd) {
+    RECT clientRect;
+    GetClientRect(hWnd, &clientRect);
+
+    if (imageX < 0) imageX = 0;
+    if (imageY < 0) imageY = 0;
+    if (imageX + IMAGE_WIDTH > clientRect.right) {
+        imageX = clientRect.right - IMAGE_WIDTH;
+    }
+    if (imageY + IMAGE_HEIGHT > clientRect.bottom) {
+        imageY = clientRect.bottom - IMAGE_HEIGHT;
+    }
 }
 
 // Обработчик сообщений окна
@@ -54,9 +89,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
 
-            // Создаем контекст устройства для памяти (double buffering)
             HDC memDC = CreateCompatibleDC(hdc);
-            HBITMAP memBitmap = CreateCompatibleBitmap(hdc, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top);
+            HBITMAP memBitmap = CreateCompatibleBitmap(hdc, ps.rcPaint.right - ps.rcPaint.left,
+                                                       ps.rcPaint.bottom - ps.rcPaint.top);
             SelectObject(memDC, memBitmap);
 
             Graphics graphics(memDC);
@@ -65,51 +100,134 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
             FillRect(memDC, &ps.rcPaint, hBrush);
             DeleteObject(hBrush);
 
-            // Рисуем изображение: исходное или измененное, в зависимости от состояния
+            float centerX = imageX + IMAGE_WIDTH / 2.0f;
+            float centerY = imageY + IMAGE_HEIGHT / 2.0f;
+
+            graphics.TranslateTransform(centerX, centerY);
+            graphics.RotateTransform(angle);
+            graphics.TranslateTransform(-centerX, -centerY);
+
             if (isImageChanged && clickedImage) {
                 graphics.DrawImage(clickedImage, imageX, imageY, IMAGE_WIDTH, IMAGE_HEIGHT);
             } else if (normalImage) {
                 graphics.DrawImage(normalImage, imageX, imageY, IMAGE_WIDTH, IMAGE_HEIGHT);
             }
 
-            // Копируем результат из памяти на экран
-            BitBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, memDC, 0, 0, SRCCOPY);
 
-            // Освобождаем ресурсы
+
+            BitBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left,
+                   ps.rcPaint.bottom - ps.rcPaint.top, memDC, 0, 0, SRCCOPY);
+
             DeleteObject(memBitmap);
             DeleteDC(memDC);
 
             EndPaint(hWnd, &ps);
             break;
         }
+        case WM_ERASEBKGND:
+            return 1;
         case WM_LBUTTONDOWN: {
-            // Когда пользователь нажимает на изображение, переключаемся на другое
             int mouseX = LOWORD(lParam);
             int mouseY = HIWORD(lParam);
             if (mouseX >= imageX && mouseX <= imageX + IMAGE_WIDTH &&
                 mouseY >= imageY && mouseY <= imageY + IMAGE_HEIGHT) {
                 isImageChanged = true;
                 isImageDragging = true;
-                InvalidateRect(hWnd, NULL, TRUE); // Перерисовываем окно
             }
+            UpdateSpritePosition(hWnd);
             break;
         }
         case WM_LBUTTONUP: {
-            // Когда пользователь отпускает мышь, возвращаем исходное изображение
             isImageChanged = false;
             isImageDragging = false;
-            InvalidateRect(hWnd, NULL, TRUE); // Перерисовываем окно
+            UpdateSpritePosition(hWnd);
             break;
         }
         case WM_MOUSEMOVE: {
             if (isImageDragging) {
-                imageX = LOWORD(lParam) - DRAGGING_OFFSET;  // Простое смещение, если перемещаем объект
+                imageX = LOWORD(lParam) - DRAGGING_OFFSET;
                 imageY = HIWORD(lParam) - DRAGGING_OFFSET;
-                InvalidateRect(hWnd, NULL, TRUE); // Перерисовываем окно
+
+                CheckWindowBounds(hWnd);
+
+                UpdateSpritePosition(hWnd);
             }
             break;
         }
+        case WM_KEYDOWN: {
+//TODO change
+            if (!IsEnglishKeyboardLayout()) {
+                break;
+            }
+
+            switch (wParam) {
+                case 'W':  // Вверх
+                case VK_UP:
+                    imageY -= MOVE_SPEED;
+                    break;
+                case 'S':  // Вниз
+                case VK_DOWN:
+                    imageY += MOVE_SPEED;
+                    break;
+                case 'A':  // Влево
+                case VK_LEFT:
+                    imageX -= MOVE_SPEED;
+                    break;
+                case 'D':  // Вправо
+                case VK_RIGHT:
+                    imageX += MOVE_SPEED;
+                    break;
+                case VK_ESCAPE: {
+                    int result = MessageBox(hWnd, "Are you sure you want to exit", "Confirmation of exit", MB_YESNO | MB_ICONQUESTION);
+                    if (result == IDYES) {
+                        PostQuitMessage(0);
+                    }
+                    break;
+                }
+            }
+
+            CheckWindowBounds(hWnd);
+
+            UpdateSpritePosition(hWnd);
+            break;
+        }
+        case WM_MOUSEWHEEL: {
+            int zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+
+            if (GetKeyState(VK_SHIFT) & 0x8000) {
+                imageX += (zDelta > 0 ? -MOVE_SPEED : MOVE_SPEED);
+            } else {
+                imageY += (zDelta > 0 ? -MOVE_SPEED : MOVE_SPEED);
+            }
+
+            CheckWindowBounds(hWnd);
+            UpdateSpritePosition(hWnd);
+            break;
+        }
+        case WM_COMMAND: {
+            switch (LOWORD(wParam)) {
+                case ID_ACCEL_LEFT:
+                    angle -= 5;
+                    if (angle < 0) angle += 360;
+                    break;
+                case ID_ACCEL_RIGHT:
+                    angle += 5;
+                    if (angle >= 360) angle -= 360;
+                    break;
+            }
+
+            UpdateSpritePosition(hWnd);
+            break;
+        }
         case WM_DESTROY: {
+
+            if (normalImage) {
+                delete normalImage;
+            }
+            if (clickedImage) {
+                delete clickedImage;
+            }
+
             PostQuitMessage(0);
             break;
         }
@@ -150,11 +268,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return -1;
     }
 
+    HACCEL hAccel = CreateAcceleratorTable(accelTable, sizeof(accelTable) / sizeof(ACCEL));
+
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
     while (GetMessage(&msg, nullptr, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        if (!TranslateAccelerator(hWnd, hAccel, &msg)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
     }
 
     ShutdownGDIPlus(&gdiplusToken);
