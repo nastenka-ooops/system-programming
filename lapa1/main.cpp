ef3629e4a367140ccc7caf04247db14095253f1f
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-09-11 22:58:55 +0300

    add checking bounds of rotated triangle image

--- lapa1/main.cpp
+++ lapa1/main.cpp
@@ -25,11 +25,13 @@ const int DRAGGING_OFFSET = 50;
 const int MOVE_SPEED = 10;
 const std::wstring PATH_TO_OPEN_MOUTH_CAT_IMAGE = L"C:\\Users\\madam\\CLionProjects\\sys-prog\\lapa1\\resources\\images\\catWithOpenMouse.jpg";
 const std::wstring PATH_TO_CLOSE_MOUTH_CAT_IMAGE = L"C:\\Users\\madam\\CLionProjects\\sys-prog\\lapa1\\resources\\images\\catWithCLoseMouse.jpg";
+const std::wstring PATH_TO_TRIANGLE_IMAGE = L"C:\\Users\\madam\\CLionProjects\\sys-prog\\lapa1\\resources\\images\\triangle.png";
 
 Image *normalImage = nullptr;
 Image *clickedImage = nullptr;
 bool isImageDragging = false;
 bool isImageChanged = false;
+bool isTriangleImage = false;
 
 int imageCenterX = 150;
 int imageCenterY = 150;
@@ -40,13 +42,11 @@ Image *LoadImageFromFile(const std::wstring &filePath) {
     return new Image(filePath.c_str());
 }
 
-// Инициализация GDI+
 void InitGDIPlus(ULONG_PTR *gdiplusToken) {
     GdiplusStartupInput gdiplusStartupInput;
     GdiplusStartup(gdiplusToken, &gdiplusStartupInput, nullptr);
 }
 
-// Завершение работы с GDI+
 void ShutdownGDIPlus(ULONG_PTR *gdiplusToken) {
     GdiplusShutdown(*gdiplusToken);
 }
@@ -57,7 +57,7 @@ void UpdateSpritePosition(HWND hWnd) {
 
 bool IsEnglishKeyboardLayout() {
     HKL layout = GetKeyboardLayout(0);
-    return LOWORD(layout) == 0x409;
+    return LOWORD(layout) == MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US);
 }
 
 float findMinElement(float arr[], int size) {
@@ -84,9 +84,21 @@ float findMaxElement(float arr[], int size) {
     return maxElement;
 }
 
+double TriangleArea(POINT A, POINT B, POINT C) {
+    return 0.5 * abs(A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
+}
+
+bool IsPointInTriangle(POINT P, POINT A, POINT B, POINT C) {
+    double areaABC = TriangleArea(A, B, C);
+
+    double areaPAB = TriangleArea(P, A, B);
+    double areaPBC = TriangleArea(P, B, C);
+    double areaPCA = TriangleArea(P, C, A);
+
+    return fabs(areaABC - (areaPAB + areaPBC + areaPCA)) < 1e-5;
+}
 
 void RotatePoints(float &x, float &y, int centerX, int centerY, int angle) {
-
     float angleRadians = angle * static_cast<float>(M_PI) / 180.0f;
 
     float dx = x - centerX;
@@ -102,7 +114,7 @@ void RotatePoints(float &x, float &y, int centerX, int centerY, int angle) {
     y = newY + centerY;
 }
 
-bool CheckMouseCoordinates(float mouseX, float mouseY) {
+bool CheckMouseCoordinatesForSquare(float mouseX, float mouseY) {
     RotatePoints(mouseX, mouseY, imageCenterX, imageCenterY, -angle);
 
     return (mouseX >= imageCenterX - IMAGE_WIDTH / 2 && mouseX <= imageCenterX + IMAGE_WIDTH / 2 &&
@@ -151,7 +163,37 @@ void CheckRotatedSquareBounds(HWND hWnd) {
 
 }
 
-// Обработчик сообщений окна
+void calculateTrianglePoints(POINT points[3], int centerX, int centerY, double angle) {
+
+    points[0] = {0, (LONG) (-IMAGE_HEIGHT / 2)};
+    points[1] = {(LONG) (-IMAGE_WIDTH / 2), (LONG) (IMAGE_HEIGHT / 2)};
+    points[2] = {(LONG) (IMAGE_WIDTH / 2), (LONG) (IMAGE_HEIGHT / 2)};
+
+    float angleRadians = angle * static_cast<float>(M_PI) / 180.0f;
+
+    for (int i = 0; i < 3; ++i) {
+        double x = points[i].x * cos(angleRadians) - points[i].y * sin(angleRadians);
+        double y = points[i].x * sin(angleRadians) + points[i].y * cos(angleRadians);
+        points[i].x = (LONG) (centerX + x);
+        points[i].y = (LONG) (centerY + y);
+    }
+}
+
+void CheckTriangleBounds(HWND hWnd) {
+    RECT clientRect;
+    GetClientRect(hWnd, &clientRect);
+
+    POINT points[3];
+    calculateTrianglePoints(points, imageCenterX, imageCenterY, angle);
+
+    for (int i = 0; i < 3; ++i) {
+        if (points[i].x < 0) imageCenterX += MOVE_SPEED;
+        if (points[i].x > clientRect.right) imageCenterX -= MOVE_SPEED;
+        if (points[i].y < 0) imageCenterY += MOVE_SPEED;
+        if (points[i].y > clientRect.bottom) imageCenterY -= MOVE_SPEED;
+    }
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
                          WPARAM wParam, LPARAM lParam) {
     switch (message) {
@@ -206,12 +248,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
         case WM_ERASEBKGND:
             return 1;
         case WM_LBUTTONDOWN: {
+            POINT clickPoint;
+            clickPoint.x = LOWORD(lParam);
+            clickPoint.y = HIWORD(lParam);
+
             int mouseX = LOWORD(lParam);
             int mouseY = HIWORD(lParam);
 
-            if (CheckMouseCoordinates(mouseX, mouseY)) {
-                isImageChanged = true;
-                isImageDragging = true;
+            POINT points[3];
+            calculateTrianglePoints(points, imageCenterX, imageCenterY, angle);
+
+            if (isTriangleImage) {
+
+                if (IsPointInTriangle(clickPoint, points[0], points[1], points[2])) {
+                    isImageChanged = true;
+                    isImageDragging = true;
+                };
+            } else {
+                if (CheckMouseCoordinatesForSquare(mouseX, mouseY)) {
+                    isImageChanged = true;
+                    isImageDragging = true;
+                }
             }
             UpdateSpritePosition(hWnd);
             break;
@@ -227,8 +284,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
                 imageCenterX = LOWORD(lParam) - DRAGGING_OFFSET;
                 imageCenterY = HIWORD(lParam) - DRAGGING_OFFSET;
 
-                CheckRotatedSquareBounds(hWnd);
-
+                if (isTriangleImage) {
+                    CheckTriangleBounds(hWnd);
+                } else {
+                    CheckRotatedSquareBounds(hWnd);
+                }
                 UpdateSpritePosition(hWnd);
             }
             break;
@@ -240,19 +300,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
             }
 
             switch (wParam) {
-                case 'W':  // Вверх
+                case 'T':
+                    normalImage = LoadImageFromFile(PATH_TO_TRIANGLE_IMAGE);
+                    isTriangleImage = true;
+                    break;
+                case 'C':
+                    normalImage = LoadImageFromFile(PATH_TO_CLOSE_MOUTH_CAT_IMAGE);
+                    isTriangleImage = false;
+                    break;
+                case 'W':
                 case VK_UP:
                     imageCenterY -= MOVE_SPEED;
                     break;
-                case 'S':  // Вниз
+                case 'S':
                 case VK_DOWN:
                     imageCenterY += MOVE_SPEED;
                     break;
-                case 'A':  // Влево
+                case 'A':
                 case VK_LEFT:
                     imageCenterX -= MOVE_SPEED;
                     break;
-                case 'D':  // Вправо
+                case 'D':
                 case VK_RIGHT:
                     imageCenterX += MOVE_SPEED;
                     break;
@@ -266,8 +334,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
                 }
             }
 
-            CheckRotatedSquareBounds(hWnd);
-
+            if (isTriangleImage) {
+                CheckTriangleBounds(hWnd);
+            } else {
+                CheckRotatedSquareBounds(hWnd);
+            }
             UpdateSpritePosition(hWnd);
             break;
         }
@@ -280,8 +351,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
                 imageCenterY += (zDelta > 0 ? -MOVE_SPEED : MOVE_SPEED);
             }
 
-            CheckRotatedSquareBounds(hWnd);
-
+            if (isTriangleImage) {
+                CheckTriangleBounds(hWnd);
+            } else {
+                CheckRotatedSquareBounds(hWnd);
+            }
             UpdateSpritePosition(hWnd);
             break;
         }
@@ -318,7 +392,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
     return 0;
 }
 
-// Точка входа
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     ULONG_PTR gdiplusToken;
     InitGDIPlus(&gdiplusToken);
